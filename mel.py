d96a3bd9e023b3ffedfa807cb1ec20d829294c6a
jesse emerson <mrloquacious@gmail.com>
2020-05-19 16:39:14 -0700

    Add envelope.

--- mel.py
+++ mel.py
@@ -12,16 +12,17 @@ import sys
 
 SAMPLE_RATE = 24000
 
-SECONDS = 1
+SECONDS = .05
 FREQ_MIN = 100
 FREQ_MAX = 1000
 NUM_NOTES = 100
 
-currentNotes = set()
+ATTACK_TIME = .01
+ATTACK = int(ATTACK_TIME * SAMPLE_RATE)
+RELEASE_TIME = .01
+RELEASE = int(RELEASE_TIME * SAMPLE_RATE)
 
-#notes = [i for i in np.linspace(FREQ_MIN, FREQ_MAX, NUM_NOTES - 1)] 
-notes = [random.randint(FREQ_MIN, FREQ_MAX) for i in range(NUM_NOTES)]
-#random.randint(FREQ_MIN, FREQ_MAX)
+currentNotes = set()
 
 # Create a dataframe from csv
 df = pd.read_csv('freq_12tone.csv', delimiter=',')
@@ -31,21 +32,27 @@ twelveTone = [list(row) for row in df.values]
 twelveTone = twelveTone[30:54]
 notes = [random.choice(twelveTone) for i in range(NUM_NOTES)]
 
-def calcFreq():
-    return 440 * 2**((key - 69) / 12)
-
 def calcSine(notes, SECONDS):
     #SAMPLE_RATE = 24000
-    NUM = math.trunc( SECONDS * SAMPLE_RATE)
+    NUM = math.trunc(SECONDS * SAMPLE_RATE)
 
     for freq in notes:
         x = np.linspace(0, SECONDS, NUM, False)
         y =  np.sin(x * freq * 2 * np.pi)
-        audio = y * (2**15 - 1) / np.max(np.abs(y))
-        audio = audio.astype(np.int16)
-        #playSine(audio)
-    return audio
+        # Add the attack envelope:
+        att_env = np.linspace(0, 1, ATTACK, False)
+        ones = np.ones(NUM - ATTACK - RELEASE)
+        rel_env = np.linspace(1, 0, RELEASE, False)
+        att_env = np.append(att_env, ones)
+        att_env = np.append(att_env, rel_env)
 
+        audio = att_env * y * (2**15 - 1) / np.max(np.abs(y))
+        audio = audio.astype(np.int16)
+##### SIMPLEAUDIO PLAY #####
+        playSine(audio)
+##### END SIMPLEAUDIO PLAY #####
+
+    return 0
 
 def playSine(audio):
     # Example of simpleaudio.play_buffer:
@@ -54,39 +61,38 @@ def playSine(audio):
     play = sa.play_buffer(audio, 1, 2, 24000)
     play.wait_done();
 
-def callback(in_data, frame_count, time_info, status):
-    #data = wf.readframes(frame_count)
-    data = calcSine(notes, SECONDS)
+### TEST ###
+calcSine(notes, SECONDS)
+### END TEST ###
 
-    return (data, pyaudio.paContinue)
-
-#randMel()
-
-
-# Create an interface to PortAudio
-pa = pyaudio.PyAudio()
-
-# Set up the audio output stream.
-# 'output = True' indicates that the sound will be played rather than recorded
-stream = pa.open(
-    format = pa.get_format_from_width(2),
-    channels = 1,
-    rate = SAMPLE_RATE,
-    output = True,
-    stream_callback = callback)
-
-
-# start the stream (4)
-stream.start_stream()
-
-# wait for stream to finish (5)
-while stream.is_active():
-    time.sleep(0.1)
-
-
-# Done, clean up and exit.
-stream.stop_stream()
-stream.close()
-
-# close PyAudio (7)
-#pa.terminate()
+#def callback(in_data, frame_count, time_info, status):
+#    #data = wf.readframes(frame_count)
+#    data = calcSine(notes, SECONDS)
+#
+#    return (data, pyaudio.paContinue)
+#
+#
+###### PYAUDIO #####
+## Create an interface to PortAudio
+#pa = pyaudio.PyAudio()
+#
+## Set up the audio output stream.
+## 'output = True' indicates that the sound will be played rather than recorded
+#stream = pa.open(
+#    format = pa.get_format_from_width(2),
+#    channels = 1,
+#    rate = SAMPLE_RATE,
+#    output = True,
+#    stream_callback = callback)
+#
+#stream.start_stream()
+#
+#while stream.is_active():
+#    time.sleep(0.1)
+#
+## Done, clean up and exit.
+#stream.stop_stream()
+#stream.close()
+#
+## close PyAudio (7)
+##pa.terminate()
